6e10e53e76d9424fee3db272a4699b1328255d7f
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2023-12-30 00:53:20 +0000

    end game added and also changed to black hole type game

--- snake-script.js
+++ snake-script.js
@@ -1,158 +1,163 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Direction flags
-    let currentDirection = { dx: 0, dy: 0 };
-    let mainCirclePositions = []; // Array to store previous positions of main circle
+    // Create the main circle in the center of the page
+    const mainCircle = document.createElement("div");
+    mainCircle.classList.add("mainCircleAnimation");
+    mainCircle.style.top = "50%";
+    mainCircle.style.left = "50%";
+    mainCircle.style.width = '5px';
+    mainCircle.style.height = '5px';
+    document.body.appendChild(mainCircle);
 
+    let points = 0;
+    let previousPoints = 0;
+
+    // Position variables
+    let posX = window.innerWidth / 2;
+    let posY = window.innerHeight / 2;
+
+    // Direction variables
+    let dx = 0;  // horizontal movement
+    let dy = 0;  // vertical movement
+
+    // Initialize a counter for flashing circles
+    let flashingCircleCount = 0;
+
+    // Function to calculate distance between two points
+    function calculateDistance(x1, y1, x2, y2) {
+        return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    }
+
+    // Function to check for collision and remove flashing circles
+    function checkCollisionAndRemove() {
+        const mainCircleX = posX;
+        const mainCircleY = posY;
+        const mainCircleRadius = 25;
     
-    function createMainCircle() {
-        const circle = document.createElement("div");
-        circle.id = 'main-circle';
-        circle.classList.add("circleAnimation");
-        circle.style.position = 'absolute';
-        circle.style.width = '1.5vw';
-        circle.style.height = '2vh'; 
-        circle.style.borderRadius = '50%';
-        circle.style.backgroundImage = 'radial-gradient(circle at 30% 30%, rgba(255, 255, 255, 0.9), transparent 40%), radial-gradient(circle at 70% 70%, rgba(255, 255, 255, 0.9), transparent 40%), radial-gradient(circle at 50% 50%, rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0) 60%)';
-        document.body.appendChild(circle);
-    }
+        const flashingCircles = document.querySelectorAll('.flashAnimation');
+        flashingCircles.forEach(flashingCircle => {
+            const flashingCircleX = parseFloat(flashingCircle.style.left) + flashingCircle.offsetWidth / 2;
+            const flashingCircleY = parseFloat(flashingCircle.style.top) + flashingCircle.offsetHeight / 2;
+            const flashingCircleRadius = 25;
 
-    createMainCircle(); // Create the main circle dynamicall
+            const distance = calculateDistance(mainCircleX, mainCircleY, flashingCircleX, flashingCircleY);
 
-    // Array to store divs
-    const divs = [];
-    const collidedQueue = []; // Queue to maintain the order of collided divs
+            if (distance < mainCircleRadius + flashingCircleRadius) {
+                flashingCircle.remove();
+                flashingCircleCount--;
+                points++;
 
-    function createCircles() {
-        const totalDivs = 20;
-        let divCount = 0;
+                // Update the points displayed on the screen
+                document.getElementById('points').innerText = points;
 
-        function createDivWithDelay() {
-            if (divCount < totalDivs) {
-                const div = document.createElement("div");
-                div.classList.add("circleAnimation", "flashAnimation");
-                div.dataset.index = divCount;
-                div.style.top = `${Math.random() * 90}vh`;
-                div.style.left = `${Math.random() * 90}vw`;
-                document.body.appendChild(div);
-                divs.push({ element: div, isFollowing: false });
-
-                // Add the first three divs to the collidedQueue
-            if (divCount < 3) {
-                collidedQueue.push(divCount);
-                divs[divCount].isFollowing = true; // Set the flag to follow the main circle
-            }
-
-                divCount++;
-                setTimeout(createDivWithDelay, 500);
-            }
-        }
-        createDivWithDelay();
-    }
-
-    createCircles();
-
-    function moveCircle() {
-        const circle = document.getElementById('main-circle');
-        const rect = circle.getBoundingClientRect();
-
-        // Store previous position of main circle
-        mainCirclePositions.push({ top: rect.top, left: rect.left });
-        if (mainCirclePositions.length > 20) { // Adjust this number based on how many positions you want to remember
-            mainCirclePositions.shift(); // Remove the oldest position
-        }
-
-        let newTop = rect.top + currentDirection.dy;
-        let newLeft = rect.left + currentDirection.dx;
-
-        const screenWidth = window.innerWidth;
-        const screenHeight = window.innerHeight;
-
-        if (newTop < 0) newTop = screenHeight;
-        else if (newTop > screenHeight) newTop = 0;
-
-        if (newLeft < 0) newLeft = screenWidth;
-        else if (newLeft > screenWidth) newLeft = 0;
-
-        circle.style.top = `${newTop}px`;
-        circle.style.left = `${newLeft}px`;
-
-        collidedQueue.forEach((index, position) => {
-            if (mainCirclePositions.length > position) {
-                const { top, left } = mainCirclePositions[mainCirclePositions.length - 1 - position];
-                const data = divs[index];
-                const div = data.element;
-                const delay = position * 200; // Delay based on position in the queue
-                setTimeout(() => {
-                    div.style.top = `${top}px`;
-                    div.style.left = `${left}px`;
-                }, delay);
+                // Increase the width and height of mainCircle by 1 pixel
+                mainCircle.style.width = `${parseFloat(mainCircle.style.width) + 2}px`;
+                mainCircle.style.height = `${parseFloat(mainCircle.style.height) + 2}px`;
             }
         });
-
-        requestAnimationFrame(moveCircle);
     }
 
-    function checkCollision(div) {
-        const circle = document.getElementById('main-circle');
-        const circleRect = circle.getBoundingClientRect();
-        const divRect = div.getBoundingClientRect();
-        return (
-            circleRect.left < divRect.right &&
-            circleRect.right > divRect.left &&
-            circleRect.top < divRect.bottom &&
-            circleRect.bottom > divRect.top
-        );
-    }
-
-    document.addEventListener('keydown', function(event) {
+    // Function to move the circle based on arrow key presses
+    function moveCircle(event) {
         switch (event.key) {
-            case 'ArrowUp':
-                currentDirection = { dx: 0, dy: -1 };
+            case "ArrowUp":
+                dy = -10;
+                dx = 0;
                 break;
-            case 'ArrowDown':
-                currentDirection = { dx: 0, dy: 1 };
+            case "ArrowDown":
+                dy = 10;
+                dx = 0;
                 break;
-            case 'ArrowLeft':
-                currentDirection = { dx: -1, dy: 0 };
+            case "ArrowLeft":
+                dx = -10;
+                dy = 0;
                 break;
-            case 'ArrowRight':
-                currentDirection = { dx: 1, dy: 0 };
+            case "ArrowRight":
+                dx = 10;
+                dy = 0;
                 break;
+            default:
+                return;
         }
-    });
-
-    // Function to handle screen click events
-function handleScreenClick(event) {
-    const screenWidth = window.innerWidth;
-    const screenHeight = window.innerHeight;
-    const clickX = event.clientX;
-    const clickY = event.clientY;
-
-    if (clickX < screenWidth * 0.25) {  // Left quarter of the screen
-        currentDirection = { dx: -1, dy: 0 };
-    } else if (clickX > screenWidth * 0.75) {  // Right quarter of the screen
-        currentDirection = { dx: 1, dy: 0 };
-    } else if (clickY < screenHeight * 0.25) {  // Top quarter of the screen
-        currentDirection = { dx: 0, dy: -1 };
-    } else if (clickY > screenHeight * 0.75) {  // Bottom quarter of the screen
-        currentDirection = { dx: 0, dy: 1 };
     }
-}
 
-// Add click event listener to the entire window
-document.addEventListener('click', handleScreenClick);
+    // Add event listener for arrow key presses
+    document.addEventListener("keydown", moveCircle);
 
-    moveCircle(); // Start moving the main circle
+    // Function to move the circle based on touch positions
+    function moveCircleOnTouch(event) {
+        const touchX = event.touches[0].clientX;
+        const touchY = event.touches[0].clientY;
 
-    setInterval(() => {
-        divs.forEach((data, index) => {
-            const div = data.element;
-            if (checkCollision(div)) {
-                if (!collidedQueue.includes(index)) {
-                    collidedQueue.push(index); // Add to the queue if not already present
-                    data.isFollowing = true; // Set the flag to follow the main circle
-                }
-            }
-        });
-    }, 100); // Check for collision every 100ms
+        const rect = document.body.getBoundingClientRect();
+
+        if (touchX < rect.left + window.innerWidth * 0.2) {
+            dx = -10;
+            dy = 0;
+        } else if (touchX > rect.left + window.innerWidth * 0.8) {
+            dx = 10;
+            dy = 0;
+        } else if (touchY < rect.top + window.innerHeight * 0.2) {
+            dx = 0;
+            dy = -10;
+        } else if (touchY > rect.top + window.innerHeight * 0.8) {
+            dx = 0;
+            dy = 10;
+        } else {
+            return; // Return if touched elsewhere
+        }
+    }
+
+    // Add event listener for touch events on the sides and top/bottom of the screen
+    document.body.addEventListener("touchstart", moveCircleOnTouch);
+
+    // Function to update the position of the main circle
+    function updateCirclePosition() {
+        posX += dx;
+        posY += dy;
+
+        // Check boundaries and reset position if the main circle hits the border
+        if (posX < 0 || posX > window.innerWidth || posY < 0 || posY > window.innerHeight) {
+            posX = window.innerWidth / 2;
+            posY = window.innerHeight / 2;
+            dx = 0;
+            dy = 0;
+            previousPoints = points;
+            document.getElementById('previousPoints').innerText = previousPoints;
+            points = 0;
+            document.getElementById('points').innerText = points;
+            mainCircle.style.width = '5px';
+            mainCircle.style.height = '5px';
+        }
+
+        mainCircle.style.top = `${posY}px`;
+        mainCircle.style.left = `${posX}px`;
+
+        checkCollisionAndRemove();
+
+        requestAnimationFrame(updateCirclePosition);
+    }
+
+    // Start updating the circle position
+    updateCirclePosition();
+
+    // Function to create flashing circles at random positions
+    function createFlashingCircle() {
+        if (flashingCircleCount < 5) {
+            const flashingCircle = document.createElement("div");
+            flashingCircle.classList.add("circleAnimation", "flashAnimation");
+
+            const randomX = Math.random() * window.innerWidth * 0.95;
+            const randomY = Math.random() * window.innerHeight * 0.95;
+
+            flashingCircle.style.top = `${randomY}px`;
+            flashingCircle.style.left = `${randomX}px`;
+
+            document.body.appendChild(flashingCircle);
+
+            flashingCircleCount++;
+        }
+    }
+
+    // Call the function to create a flashing circle every 3 seconds
+    setInterval(createFlashingCircle, 100);
 });
